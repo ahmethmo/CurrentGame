d50042c3e190e70b9f41dcf530808a9c44fbc3cf
ahmet hacimurtezaoğlu <ahmethmo@gmail.com>
2015-11-16 10:23:57 +0200

    Revert "ilk"
    This reverts commit bc827a7f6e82e6501052c2ffeede32cc6de4227c.

--- CurrentGame/src/main/java/Ara/Jsons/test.java
+++
@@ -1,8 +0,0 @@
-package Ara.Jsons;
-
-public class test {
-    public static void main(String[] args) {
-        CurrentGameJson a=new CurrentGameJson();
-        a.GetCurrentgame("Emta", "EUW");
-    }
-}
